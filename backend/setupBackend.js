84182e99cde60bebe48a92e3ed3002d8c84b3673
lisamarimyrene <lisamy@stud.ntnu.no>
2023-11-28 14:40:05 +0100

    ?

--- backend/setupBackend.js
+++
@@ -1,6 +0,0 @@
-// Run npm install
-const { execSync } = require('child_process');
-execSync('npm install', { stdio: 'inherit' });
-
-// Run the server script and display in console
-execSync('npm run server', { stdio: 'inherit' });
